09f4cf445becb5248bd07e262e7caa356a7ab28f
Chris Dalley <chris@collaborate.co.uk>
2013-01-25 15:22:56 +0000

    Initital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collab::Application.config.secret_token = '24940580f01879e0ce8f0e114754f2bb9b06a5746fdc9abe420206389474deb8bf7a9a11b2f191e86fcccb3e455b2272c6dd2872bc4d6037b43706d276efbbfc'
